193f286786cc94c5628f5d0cbfede7a5cf0a9120
Wilson Santos <contato@wilsonsantos.net>
2023-12-13 12:22:20 -0300

    Add Domain

---
+++ WS.EstacionamentoHexagonal.Domain/Entities/Permanencia.cs
@@ -0,0 +1,39 @@
+﻿namespace WS.EstacionamentoHexagonal.Domain.Entities
+{
+    public class Permanencia(Veiculo veiculo, DateTime dataEntrada)
+    {
+        public Veiculo Veiculo { get; private set; } = veiculo;
+        public DateTime DataEntrada { get; private set; } = dataEntrada;
+
+        public DateTime DataSaida { get; private set; } = DateTime.MinValue;
+
+        public void RegistrarSaida(DateTime dataSaida)
+        {
+            if (dataSaida < DataEntrada)
+                throw new InvalidOperationException(
+                    "Data de saida não pode ser menor que a de entrada"
+                    );
+
+            DataSaida = dataSaida;
+
+        }
+
+        public TimeSpan CalculaTempoPermanencia()
+        {
+            if (DataSaida == DateTime.MinValue)
+                throw new InvalidOperationException(
+                   "Calculo de tempo de permanencia não pode ser chamado sem data de saida"
+                   );
+
+            var tempoPermanencia = DataSaida - DataEntrada;
+
+            return tempoPermanencia;
+        }
+
+        public Double CalculaValorPermanencia(double valorHora)
+        {
+            var permanencia = CalculaTempoPermanencia();
+            return permanencia.TotalHours * valorHora;
+        }
+    }
+}
---
+++ WS.EstacionamentoHexagonal.Domain/Entities/Veiculo.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WS.EstacionamentoHexagonal.Domain.Entities
+{
+    public partial class Veiculo
+    {
+        private static readonly Regex regexPlaca =
+            RegexPlaca();
+
+        [GeneratedRegex(@"^[A-Z]{3}-?[0-9]{4}$|^[A-Z]{3}[0-9][A-Z][0-9]{2}$", RegexOptions.Compiled)]
+        private static partial Regex RegexPlaca();
+
+        public string Placa { get; set; } = string.Empty;
+
+        public Veiculo(string placa)
+        {
+           
+
+            if (string.IsNullOrEmpty(placa.Trim()))
+                throw new ArgumentException("A placa não pode ser nula ou vazia", nameof(placa));
+
+            if (!regexPlaca.IsMatch(placa.Trim()))
+                throw new ArgumentException("A placa não segue o formato válido", nameof(placa));
+
+            Placa = placa.Trim();
+        }
+
+    }
+}
+
+
