05fdbfd09bb5a3fa49a3f9547bb3c543a2d89387
Wilson Santos <contato@wilsonsantos.net>
2024-02-21 10:27:22 -0300

    Adicionado o filtro para não serializar objetos nulos
    ps: estava aprendendo sobre, só quis registrar

--- WS.EstacionamentoHexagonal.Api/Program.cs
+++ WS.EstacionamentoHexagonal.Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.DefaultIgnoreCondition =
+            JsonIgnoreCondition.WhenWritingNull;
+    });
+
 
 var app = builder.Build();
 
